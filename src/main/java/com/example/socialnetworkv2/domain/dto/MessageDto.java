f1544fab43b987f45606ff8dbf2f1753a17d57aa
Arsqq <arny723@gmail.com>
2021-11-28 20:14:59 +0300

    predfinal com

---
+++ src/main/java/com/example/socialnetworkv2/domain/dto/MessageDto.java
@@ -0,0 +1,43 @@
+package com.example.socialnetworkv2.domain.dto;
+
+import com.example.socialnetworkv2.domain.Message;
+import com.example.socialnetworkv2.domain.User;
+import com.example.socialnetworkv2.domain.utils.MessageHelper;
+import lombok.Data;
+
+@Data
+public class MessageDto {
+    private Long id;
+    private String text;
+    private String tag;
+    private User author;
+    private String filename;
+    private Long likes;
+    private Boolean meLiked;
+
+    public MessageDto(Message message, Long likes, Boolean meLiked) {
+        this.id = message.getId();
+        this.text = message.getText();
+        this.tag = message.getTag();
+        this.author = message.getAuthor();
+        this.filename = message.getFilename();
+        this.likes = likes;
+        this.meLiked = meLiked;
+    }
+
+    public String getAuthorName(){
+        return MessageHelper.getAuthorName(author);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "MessageDto{" +
+                "id=" + id +
+                ", author=" + author +
+                ", likes=" + likes +
+                ", meLiked=" + meLiked +
+                '}';
+    }
+}
